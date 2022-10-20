50ae43cb7a30e79426b7a1c8c8b8cc5bb61b755b
Alex Zenla <alex@fork.run>
2022-10-20 01:31:25 -0500

    Initial Commit

---
+++ Formula/metalgpu.rb
@@ -0,0 +1,16 @@
+class Metalgpu < Formula
+  desc "View Metal GPU information from the command-line"
+  homepage "https://github.com/mysticlgbt/metalgpu"
+
+  url "https://github.com/mysticlgbt/metalgpu/releases/download/v0.0.2/metalgpu"
+  sha256 "def8d350bcc6cf645d95f91393eb36b139e7a230bc639df54e0662f44c69478a"
+  license "MIT"
+
+  def install
+    bin.install Dir["*"]
+  end
+
+  test do
+    system "#{bin}/metalgpu"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Mystic on Homebrew
+
+```
+$ brew tap mysticlgbt/made
+```
+
+## Contents
+
+- [asset-cache-tool](Formula/asset-cache-tool.rb): Interact with the Apple Content Cache
+- [metalgpu](Formula/metalgpu.rb): View Metal GPU information
