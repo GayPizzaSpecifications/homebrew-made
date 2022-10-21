2f4012be227861194e7ed410f62b136eafaff14b
Alex Zenla <alex@fork.run>
2022-10-20 19:41:31 -0500

    diavirt: 0.0.3

---
+++ Formula/diavirt.rb
@@ -0,0 +1,16 @@
+class Diavirt < Formula
+  desc "Do-It-All Virtualization Tool"
+  homepage "https://github.com/mysticlgbt/diavirt"
+
+  url "https://github.com/mysticlgbt/diavirt/releases/download/v0.0.3/diavirt"
+  sha256 "2a209884945f9cb574d777815ce27b326769c6f685931c69690d82a08ecc5207"
+  license "MIT"
+
+  def install
+    bin.install Dir["*"]
+  end
+
+  test do
+    system "#{bin}/diavirt", "--help"
+  end
+end
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ $ brew tap mysticlgbt/made
 ## Contents
 
 - [asset-cache-tool](Formula/asset-cache-tool.rb): Interact with the Apple Content Cache
+- [diavirt](Formula/diavirt.rb): Do-It-All Virtualization Tool for macOS
 - [metalgpu](Formula/metalgpu.rb): View Metal GPU information
